e80bfbaba6f0538184d4ecef9b1ad4786366a265
LiamGunning07 <liamgunning07@gmail.com>
2023-12-20 14:43:20 -0700

    Files added or changed for EJS implementation

--- express_server.js
+++ express_server.js
@@ -1,5 +1,28 @@
 const express = require("express");
 const app = express();
+
+app.set('view engine', 'ejs');
+// index page
+app.get('/', function(req, res) {
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
+
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
@@ -16,6 +39,14 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
