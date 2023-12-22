0e292631796fbea085c0979e1fab8182749aad38
LiamGunning07 <liamgunning07@gmail.com>
2023-12-22 11:54:10 -0700

    Post request for new URL implemented and function def for random string defined in global scope

--- express_server.js
+++ express_server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
+const generateRandomString = () => Math.random().toString(16).substr(6,6);
+// Testing GRS: console.log(generateRandomString(6));
 
 // index page
 app.get('/', function(req, res) {
@@ -34,6 +37,13 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
